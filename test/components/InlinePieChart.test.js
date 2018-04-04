5175a65d1a42b94a1f7d6f0c9a7119fb06e48e70
Joao Menighin <joao.menighin@accenture.com>
2018-04-03 23:22:06 -0300

    Vue Testing with Jest - In Progress

---
+++ test/components/InlinePieChart.test.js
@@ -0,0 +1,8 @@
+import { mount } from 'vue-test-utils';
+// import InlinePieChart from '../../src/components/InlinePieChart.vue';
+
+describe('InlinePieChart', () => {
+    it('Tests jest', () => {
+      expect(true).toBe(true);
+    });
+});
