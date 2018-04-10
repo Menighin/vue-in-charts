62b865b5435df598c557136c40150bda660277f1
Menighin <joao.menighin@gmail.com>
2018-04-09 21:58:41 -0300

    InlineLineChart - Rendering the 0 line + Padding + dynamic Y Axis

--- src/components/InlineLineChart.vue
+++ src/components/InlineLineChart.vue
@@ -4,12 +4,15 @@
             class="root"
             ref="svg"
             :width="width"
-            :height="height">
+            :height="height"
+            :style="`padding-top: ${paddingTop}; padding-right: ${paddingRight}; padding-bottom: ${paddingBottom}; padding-left: ${paddingLeft};`">
 
-            <polyline :points="points" stroke="black" stroke-width="2" fill="none" />
+            <polyline :points="points" stroke="black" stroke-width="3" fill="none" />
+
+            <polyline :points="`0,${zeroHeight} ${width},${zeroHeight}`" stroke="#aaa" stroke-width="1" fill="none" stroke-dasharray="1, 2" />
 
             <g>
-                <circle v-for="(p, i) in points" :key="`point-${i}`" :cx="p[0]" :cy="p[1]" r="2" fill="blue" />
+                <circle v-for="(p, i) in points" :key="`point-${i}`" :cx="p[0]" :cy="p[1]" r="3" fill="blue" />
             </g>
 
         </svg>
@@ -20,9 +23,20 @@
 <script>
     export default {
         props: {
-            width:  { type: String, default: '250' },
-            height: { type: String, default: '100' },
-            data:   { type: Array,  required: true }
+            width:         { type: String, default: '250'  },
+            height:        { type: String, default: '100'  },
+            data:          { type: Array,  required: true  },
+            minY:          { type: Number, default:  null  },
+            maxY:          { type: Number, defaukt:  null  },
+            paddingTop:    { type: String, default:  '5px' },
+            paddingRight:  { type: String, default:  '5px' },
+            paddingBottom: { type: String, default:  '5px' },
+            paddingLeft:   { type: String, default:  '5px' },
+        },
+        data() {
+            return {
+                zeroHeight: 0
+            }
         },
         computed: {
             points() {
@@ -30,18 +44,22 @@
                 const w = parseInt(this.width);
                 const xStep = w / (this.data.length - 1);
 
-                let minValue = this.data.reduce((min, val) => val < min ? val : min, this.data[0]);
-                let maxValue = this.data.reduce((max, val) => val > max ? val : max, this.data[0]);
+                let minValue = this.minY || this.data.reduce((min, val) => val < min ? val : min, this.data[0]);
+                let maxValue = this.maxY || this.data.reduce((max, val) => val > max ? val : max, this.data[0]);
 
-                let yFactor = h / maxValue;
+                let yFactor = h / (maxValue - Math.min(0, minValue));
 
+                // Calculating Y positions
                 let points = [];
                 let xValue = 0;
                 this.data.forEach(d => {
-                    let yValue = h - (d * yFactor);
+                    let yValue = h - ((d - Math.min(0, minValue)) * yFactor);
                     points.push([xValue, yValue]);
                     xValue += xStep;
                 });
+
+                // Calculating the height of the zero line
+                this.zeroHeight = h - ((0 - Math.min(0, minValue)) * yFactor);
 
                 return points;
             },
@@ -53,6 +71,9 @@
 
                 return pointsStr;
             }
+        },
+        beforeMount() {
+            this.zeroHeight = this.height;
         }
     }
 </script>
@@ -61,7 +82,7 @@
 
     .inline-line-wrapper {
         .root {
-            background-color: tomato;
+            background: oldlace;
         }
     }
 
